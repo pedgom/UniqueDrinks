9adb27fb598a4dd933c3160f36a89685c9359718
Pedro Gomes <aluno21279@ipt.pt>
2021-08-24 16:28:55 +0100

    Adição de imagens, e ajustes ao ImagensController

--- UniqueDrinks/UniqueDrinks/Controllers/ImagensController.cs
+++ UniqueDrinks/UniqueDrinks/Controllers/ImagensController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -39,18 +40,19 @@ namespace UniqueDrinks.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction("Index");
             }
 
-            var imagens = await _context.Imagens
-                .Include(i => i.Bebida)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (imagens == null)
+            var imagem = await _context.Imagens
+                                         .Include(f => f.Bebida)
+                                         .FirstOrDefaultAsync(f => f.Id == id);
+            if (imagem == null)
             {
-                return NotFound();
+                
+                return RedirectToAction("Index");
             }
 
-            return View(imagens);
+            return View(imagem);
         }
 
         // GET: Imagens/Create
@@ -68,27 +70,80 @@ namespace UniqueDrinks.Controllers
         public async Task<IActionResult> Create([Bind("BebidaFK")] Imagens foto, IFormFile fotoBebida)
         {
             // avaliar se  o utilizador escolheu uma opção válida na dropdown da Bebida
-            if (foto.BebidaFK > 0)
+            if (foto.BebidaFK < 0)
             {
-                if (ModelState.IsValid)
-                {
-                    try
-                    {
-                        _context.Add(foto);
-                        await _context.SaveChangesAsync();
-                        return RedirectToAction(nameof(Index));
-                    }
-                    catch (Exception ex)
-                    {
-                        ModelState.AddModelError("", "Ocorreu um erro...");
+                // não foi escolhido uma bebida válido 
+                ModelState.AddModelError("", "Não se esqueça de escolher uma bebida...");
+                // devolver o controlo à View
+                ViewData["BebidaFK"] = new SelectList(_context.Bebidas.OrderBy(c => c.Nome), "Id", "Nome");
+                return View(foto);
+            }
+            string nomeImagem = "";
 
-                    }
-                }
+            if (fotoBebida == null)
+            {
+                // não há ficheiro
+                // adicionar msg de erro
+                ModelState.AddModelError("", "Adicione, por favor, a imagem da bebida");
+                // devolver o controlo à View
+                ViewData["BebidaFK"] = new SelectList(_context.Bebidas.OrderBy(c => c.Nome), "Id", "Nome");
+                return View(foto);
             }
             else
             {
-                // não foi escolhido uma bebida válida 
-                ModelState.AddModelError("", "Não se esqueça de escolher uma bebida...");
+                // há ficheiro. Mas, será um ficheiro válido?
+                if (fotoBebida.ContentType == "image/jpeg" || fotoBebida.ContentType == "image/png")
+                {
+                    // definir o novo nome da fotografia     
+                    Guid g;
+                    g = Guid.NewGuid();
+                    nomeImagem = foto.BebidaFK + "_" + g.ToString(); 
+                                                                  
+                    string extensao = Path.GetExtension(fotoBebida.FileName).ToLower();
+                    // agora, consigo ter o nome final do ficheiro
+                    nomeImagem = nomeImagem + extensao;
+
+                    // associar este ficheiro aos dados da Imagem da bebida
+                    foto.Imagem = nomeImagem;
+
+                    // localização do armazenamento da imagem
+                    string localizacaoFicheiro = _caminho.WebRootPath;
+                    nomeImagem = Path.Combine(localizacaoFicheiro, "fotos", nomeImagem);
+                }
+                else
+                {
+                    // ficheiro não é válido
+                    // adicionar msg de erro
+                    ModelState.AddModelError("", "Só pode escolher uma imagem para a associar à bebida");
+                    // devolver o controlo à View
+                    ViewData["BebidaFK"] = new SelectList(_context.Bebidas.OrderBy(c => c.Nome), "Id", "Nome");
+                    return View(foto);
+                }
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    // adicionar os dados da nova fotografia à base de dados
+                    _context.Add(foto);
+                    // consolidar os dados na base de dados
+                    await _context.SaveChangesAsync();
+
+                    // se cheguei aqui, tudo correu bem
+                    // vou guardar, agora, no disco rígido do Servidor a imagem
+                    using var stream = new FileStream(nomeImagem, FileMode.Create);
+                    await fotoBebida.CopyToAsync(stream);
+
+
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "Ocorreu um erro...");
+
+                }
             }
 
             ViewData["BebidaFK"] = new SelectList(_context.Bebidas.OrderBy(c => c.Nome), "Id", "Nome", foto.BebidaFK);
@@ -104,13 +159,14 @@ namespace UniqueDrinks.Controllers
                 return NotFound();
             }
 
-            var imagens = await _context.Imagens.FindAsync(id);
-            if (imagens == null)
+            var imagem = await _context.Imagens.FindAsync(id);
+            if (imagem == null)
             {
                 return NotFound();
             }
-            ViewData["BebidaFK"] = new SelectList(_context.Bebidas.OrderBy(c => c.Nome), "Id", "Nome", imagens.BebidaFK);
-            return View(imagens);
+            ViewData["BebidaFK"] = new SelectList(_context.Bebidas.OrderBy(c => c.Nome), "Id", "Nome", imagem.BebidaFK);
+            HttpContext.Session.SetInt32("NumImageEdit", imagem.Id);
+            return View(imagem);
         }
 
         // POST: Imagens/Edit/5
@@ -118,23 +174,31 @@ namespace UniqueDrinks.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Imagem,BebidaFK")] Imagens imagens)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Imagem,BebidaFK")] Imagens foto)
         {
-            if (id != imagens.Id)
+            if (id != foto.Id)
             {
                 return NotFound();
+            }
+
+            var numIdFoto = HttpContext.Session.GetInt32("NumImageEdit");
+
+            
+            if (numIdFoto == null || numIdFoto != foto.Id)
+            {
+                return RedirectToAction("Index");
             }
 
             if (ModelState.IsValid)
             {
                 try
                 {
-                    _context.Update(imagens);
+                    _context.Update(foto);
                     await _context.SaveChangesAsync();
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ImagensExists(imagens.Id))
+                    if (!ImagensExists(foto.Id))
                     {
                         return NotFound();
                     }
@@ -145,8 +209,8 @@ namespace UniqueDrinks.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["BebidaFK"] = new SelectList(_context.Bebidas, "Id", "Id", imagens.BebidaFK);
-            return View(imagens);
+            ViewData["BebidaFK"] = new SelectList(_context.Bebidas, "Id", "Id", foto.BebidaFK);
+            return View(foto);
         }
 
         // GET: Imagens/Delete/5
@@ -174,8 +238,16 @@ namespace UniqueDrinks.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var imagens = await _context.Imagens.FindAsync(id);
-            _context.Imagens.Remove(imagens);
-            await _context.SaveChangesAsync();
+            try
+            {
+                _context.Imagens.Remove(imagens);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
             return RedirectToAction(nameof(Index));
         }
 
